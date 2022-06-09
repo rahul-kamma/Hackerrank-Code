e5686ffbb0c22aa33f69083f7ee857562a2784aa
rahul-kamma <87776377+rahul-kamma@users.noreply.github.com>
2022-06-09 12:27:10 +0530

    Update Weather Observation Station 15.sql

--- SQL/Aggregation/Weather Observation Station 15.sql
+++ SQL/Aggregation/Weather Observation Station 15.sql
@@ -20,3 +20,31 @@ Write a query to find the corresponding Western Longitude value for the greatest
 SELECT ROUND(LONG_W,4)
 FROM STATION
 WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+
+weather-observation-station-18 problem
+/*
+Consider P1(a,b)  and P2(c,d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+
+Input Format
+
+The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+where LAT_N is the northern latitude and LONG_W is the western longitude
+
+*/
+ - - solution
+ select round(abs((min(lat_n)) - max(lat_n))+ abs(min(long_w)- max(long_w)),4) from station;
